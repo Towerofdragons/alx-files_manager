253766964bec254e86fe9b1ae321db1b939670f5
Mark <marknjoropge@gmail.com>
2024-12-12 07:26:29 +0000

    Fix server.js to use json

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 // Import required modules
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
 
-import router from './routes/index'
+
+const routes = require('./routes/index');
 // Initialize express app
 const app = express();
 
-// Middleware setup
-// app.use(bodyParser.json()); // Parse JSON bodies
-// app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
-// app.use(cors()); // Enable CORS
-// app.use(morgan('dev')); // Log requests to the console
 
 // Basic routes
-app.use('/', router)
+app.use('/', routes);
+app.use(express.json());
 
 // Start the server
 const PORT = process.env.PORT || 5000;
